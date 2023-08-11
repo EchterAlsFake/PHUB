2a491cdacc88168bdd7c56d5e2737437e13f30c6
Egsagon <83862309+Egsagon@users.noreply.github.com>
2023-08-11 21:20:02 +0200

    CH
    NGE

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -51,10 +51,6 @@ exclude_patterns = []
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
 
-# html_theme = "insegel"
-
-import sphinx_pdj_theme
-html_theme = 'sphinx_pdj_theme'
-html_theme_path = [sphinx_pdj_theme.get_html_theme_path()]
+html_theme = "insegel"
 
 intersphinx_mapping = {"python": ("https://docs.python.org/3", None)}
