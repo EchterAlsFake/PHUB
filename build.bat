2de4e6741a5841aa819a0db2c7b29c7b5c0fca05
Egsagon <83862309+Egsagon@users.noreply.github.com>
2023-10-03 12:15:13 +0200

    Delete build.bat

--- build.bat
+++
@@ -1,6 +0,0 @@
-:: Download dependencies and build docs
-
-py -3.11 -m pip install sphinx
-py -3.11 -m pip install -r docs/source/requirements.txt
-py -3.11 -m sphinx.cmd.build -b html docs/source docs/build
-start "" "docs/build/index.html"
