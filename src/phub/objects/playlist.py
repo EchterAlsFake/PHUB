a57869aa3266a79fb4f291f02efa60f7c20c478f
Johannes Habel <EchterAlsFake@proton.me>
2024-02-24 22:49:58 +0100

    - fixed the playlist module

--- src/phub/objects/playlist.py
+++ src/phub/objects/playlist.py
@@ -40,43 +40,51 @@ class Playlist:
         self.client = client
         self.url = url
         self.html_content = client.call(self.url).text
+        self.total_urls = self.video_urls()
 
-    @cached_property
-    def videos(self) -> Iterator[Video]:
+    def video_urls(self):
         page = 1
         has_more_videos = True
-        playlist_id = re.search(r'/playlist/(.*?)', self.url).group(1)
+        playlist_id = re.search(r'playlist/(\d+)', self.url).group(1)
         token = consts.re.get_token(self.html_content)
-
+        total_urls = []
         while has_more_videos:
             # Construct the URL for fetching the playlist page
             try:
                 params = {
-                    'id': playlist_id,  # Assume this is set up elsewhere in your class
-                    'token': token,  # Token provided at runtime
+                    'id': playlist_id,
+                    'token': token,
                     'page': page
                 }
-                # Use urlencode to properly format the query parameters for the URL
+
+                logger.info(f"Loading video page: {page}")
                 query_string = urlencode(params)
-                playlist_url = f"{self.url}?{query_string}"
-
+                playlist_url = f"https://www.pornhub.com/playlist/viewChunked?{query_string}"
                 response = self.client.call(func=playlist_url)
-
                 content = response.content.decode("utf-8")
                 video_urls = [consts.HOST + "view_video" + video for video in consts.re.get_playlist_videos(content) if
                               "pkey=" in video]
                 if video_urls:
-                    # Remove duplicates
-                    video_urls = list(set(video_urls))
-
                     for url in video_urls:
-                        video = Video(self.client, url)
-                        yield video
+                        total_urls.append(url)  # Not yielding the videos immediately, because PH will detect the high
+                        # amount of traffic and stop providing us with the next videos of the pages
+
                     page += 1  # Prepare to load the next page
                 else:
                     # No more videos found, stop the loop
+                    logger.info("No more videos found")
                     has_more_videos = False
 
-            except requests.exceptions.HTTPError:
+            except requests.exceptions.HTTPError as e:
                 has_more_videos = False
                 break
+
+            return total_urls
+
+    @cached_property
+    def videos(self) -> Iterator[Video]:
+        if len(self.total_urls) == 0 or self.total_urls is None:
+            self.video_urls()
+
+        for url in self.total_urls:
+            yield Video(url=url, client=self.client)
